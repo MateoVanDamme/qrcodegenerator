7032672792b7ac27ea21a8db25f975580567bd1d
unknown <mateovandamme1@gmail.com>
2026-04-01 00:50:32 +0200

    update look of QR code scanner

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,31 @@
-//Generates fake Guids
 function uuidv4() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
     (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16));
 }
-// Function to generate QR code
+
 function generateQRCode(str) {
     document.getElementById("qrcode").innerHTML = "";
     var size = Math.min(window.innerWidth, window.innerHeight);
     var scale = 3;
     new QRCode(document.getElementById("qrcode"), {
         text: str,
-        width: size/scale,
-        height: size/scale
+        width: size / scale,
+        height: size / scale
     });
 }
 
-document.body.addEventListener('click', generateQRCode);
-document.body.addEventListener('touchstart', generateQRCode);
+var textInput = document.getElementById("textInput");
+var generateBtn = document.getElementById("generateBtn");
+
+generateBtn.addEventListener("click", function () {
+    var text = textInput.value.trim();
+    generateQRCode(text || uuidv4());
+});
+
+textInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        generateBtn.click();
+    }
+});
 
 generateQRCode(uuidv4());
